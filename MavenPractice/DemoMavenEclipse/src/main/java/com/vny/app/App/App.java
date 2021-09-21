7995fad7b455c05af85662306411ae6aafa197f5
vny <vinay588325@gmail.com>
2021-09-21 12:50:03 +0530

    second commit

---
+++ MavenPractice/DemoMavenEclipse/src/main/java/com/vny/app/App/App.java
@@ -0,0 +1,13 @@
+package com.vny.app.App;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
